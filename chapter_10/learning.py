20f50962df0c389707fd01f491979f93f16c14e8
davrad <david@darad.net>
2019-03-18 13:41:27 +0100

    added solutions for chapter 10

---
+++ chapter_10/learning.py
@@ -0,0 +1,19 @@
+filename = 'learning_python.txt'
+
+with open(filename) as file_object:
+    #print whole file
+    contents = file_object.read().rstrip()
+    print(contents)
+    print('---')
+
+with open(filename) as file_object:
+    #print file line by line
+    for line in file_object:
+        print(line.rstrip())
+    print('---')
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
---
+++ chapter_10/learning_language.py
@@ -0,0 +1,6 @@
+filename = 'learning_python.txt'
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.replace('Python','Haskell').strip())
+    
