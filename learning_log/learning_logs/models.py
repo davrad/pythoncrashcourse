cd95c6f0feddbdac2881ec77816b7f498dbd6537
davrad <david@darad.net>
2019-03-26 16:18:24 +0100

    added code for task 18.2

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -21,4 +21,6 @@ class Entry(models.Model):
         verbose_name_plural = 'entries'
 
     def __str__(self):
+        if len(self.text) < 50:
+            return self.text
         return self.text[:50] + "..."
